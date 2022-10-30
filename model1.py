6302a16ea263c657beafcf4421a0443412834770
Felipe Yépez <58478476+FelipeYepez@users.noreply.github.com>
2022-10-29 23:59:29 -0500

    Update model1.py

--- model1.py
+++ model1.py
@@ -19,6 +19,8 @@ def reward_function(params):
     is_crashed = params['is_crashed']
     is_offtrack = params['is_offtrack']
     speed = params['speed']
+    x = params['x']
+    y = params['y']
     
     
     if(is_reversed or is_crashed):
@@ -30,26 +32,41 @@ def reward_function(params):
     
     track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
     track_direction = math.degrees(track_direction)
-    
     direction_diff = abs(track_direction - heading)
     if direction_diff > 180:
         direction_diff = 360 - direction_diff
         
     if abs(direction_diff - heading) < abs(direction_diff - (heading + steering_angle)):
+        reward *= 0.4
+    else:
+        reward *= 1.1
+    
+    
+    my_direction = math.atan2(next_point[1] - y, next_point[0] - x)
+    my_direction = math.degrees(my_direction)
+    my_diff = abs(my_direction - heading)
+    if my_diff > 180:
+        my_diff = 360 - my_diff
+        
+    if abs(my_diff - heading) < abs(my_diff - (heading + steering_angle)):
         reward *= 0.2
+    else:
+        reward *= 1.3
         
     if not all_wheels_on_track:
-        reward *= 0.2
+        reward *= 0.05
     
-    if abs(direction_diff - heading) < 6 and speed < 1.5:
-        reward *= 0.85
+    if abs(my_diff - (heading + steering_angle)) < 6 and speed < 1.5:
+        reward *= 0.8
     
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
+    marker_2 = 0.35 * track_width
     marker_3 = 0.46 * track_width
     
     if distance_from_center >= marker_3:
-        reward *= 0.3
+        reward *= 0.15
+    elif distance_from_center <= marker_2:
+        reward *= 1.2
     
     return float(reward)
